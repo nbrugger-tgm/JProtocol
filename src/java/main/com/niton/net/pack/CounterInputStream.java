f172414a84ceed62775e8d8fc368a0091f084da0
Nils Brugger <nbrugger@student.tgm.ac.at>
2020-05-04 17:40:23 +0200

    Documentation

--- src/java/main/com/niton/net/pack/CounterInputStream.java
+++ src/java/main/com/niton/net/pack/CounterInputStream.java
@@ -3,22 +3,32 @@ package com.niton.net.pack;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * A forwarding stream. Counts the read bytes 
+ */
 public class CounterInputStream extends InputStream {
 	private InputStream out;
 	private long mass;
-
+	
+	/**
+	 * @param out The stream to delegate the reads to
+	 */
 	public CounterInputStream(InputStream out) {
 		super();
 		this.out = out;
 	}
-
+	
+	/**
+	 * @return the ammount of bytes read from the underlaying stream
+	 */
 	public long getRecivedBytes() {
 		return mass;
 	}
 
 	@Override
 	public int read() throws IOException {
+		int i = out.read();
 		mass++;
-		return out.read();
+		return i;
 	}
 }
