2199c653981599735d01051c2b4ca798d4a4fa8d
Nils Brugger <nbrugger@student.tgm.ac.at>
2020-05-04 17:33:55 +0200

    Update Documentation

--- src/java/main/com/niton/net/pack/NetworkListener.java
+++ src/java/main/com/niton/net/pack/NetworkListener.java
@@ -7,25 +7,26 @@ import com.niton.net.pack.packs.Package;
 import com.niton.net.pack.requests.Request;
 
 /**
- * The NetworkListener interface provides a listener for network-packages.
+ * A listener to listen for incomming network Packages
  *
- * @author M.Marincek
+ * @author M.Marincek, Nils Brugger
  */
 
 public interface NetworkListener {
 
 	/**
-	 * Checks if the package is relevant to this listener
+	 * Checks if the package is relevant to this listener.<br>
+	 * if the package should be processed by this listener this method should return true
 	 * 
 	 * @param pack
-	 *            The package to be checked
+	 *            The package to checked
 	 * @return true, if the NetworkListener will react to this package
 	 */
 	public boolean acceptPackage(Package<? extends Serializable> pack);
 
 	/**
-	 * Handles package, if acceptPackage(Package) was true
-	 * 
+	 * Handles package, if acceptPackage(Package) was true<br>
+	 * <b>Attention<b/>Requests are not redirected to this class 
 	 * @param pack
 	 *            package to be handled
 	 * @param conection
@@ -34,12 +35,14 @@ public interface NetworkListener {
 	public void onRecivePackage(Package<? extends Serializable> pack, Socket conection);
 
 	/**
-	 * Handles requests
-	 * 
+	 * Handles requests.<br>
+	 * To not block the connection you need to send a response to the `connection` socket. With Client/Server.sendPackage.
+	 * The package needs to be of the `Package` class
+	 *
 	 * @param request
 	 *            Request to be handled
 	 * @param conection
-	 *            Socket from where the request came
+	 *            Socket from where the request came. Send the response over this socket
 	 */
 	public void onReciveRequest(Request request, Socket conection);
 }
