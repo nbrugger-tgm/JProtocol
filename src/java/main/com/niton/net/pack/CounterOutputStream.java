e10cc59a4059b577a654e14b1c9fd939321c39a9
Nils Brugger <nbrugger@student.tgm.ac.at>
2020-05-04 17:37:33 +0200

    Documentation

--- src/java/main/com/niton/net/pack/CounterOutputStream.java
+++ src/java/main/com/niton/net/pack/CounterOutputStream.java
@@ -2,23 +2,30 @@ package com.niton.net.pack;
 
 import java.io.IOException;
 import java.io.OutputStream;
-
+/**
+ * This stream counts the written bytes
+ */
 public class CounterOutputStream extends OutputStream {
 	private OutputStream out;
 	private long mass;
-
+	
+	/**
+	 * @param out the stream to forward to
+	 */
 	public CounterOutputStream(OutputStream out) {
 		super();
 		this.out = out;
 	}
-
+	/**
+	 * @return the number of bytes written to the underlaying socket
+	 */
 	public long getSendBytes() {
 		return mass;
 	}
 
 	@Override
 	public void write(int b) throws IOException {
-		mass++;
 		out.write(b);
+		mass++;
 	}
 }
